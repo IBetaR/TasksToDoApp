bb84cf4ce847a5485e9f311423a0bf164a10289a
Ilich Betancourt Rangel <ilichbr@gmail.com>
2022-07-22 10:06:48 -0300

    Project Base

---
+++ src/main/java/com/ibetar/taskstodo/TaskstodoApplication.java
@@ -0,0 +1,13 @@
+package com.ibetar.taskstodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskstodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskstodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibetar/taskstodo/TaskstodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibetar.taskstodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskstodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
